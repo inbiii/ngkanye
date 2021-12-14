3d391e04a16cbbc4a69100771a19385500071adc
inbiii <omaidksquared@gmail.com>
2021-12-14 16:54:52 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,28 @@ import { KanyeService } from './kanye.service';
 })
 export class AppComponent {
 
-  quote: string = '';
+  quotes: string[] = [];
   constructor(private yeService: KanyeService) { }
 
   ngOnInit(): void {
     const check: string[] = ['sex', 'fuck', 'porn', 'shit'];
+
     this.yeService.getYe().subscribe(payload => {
       if (check.some(el => payload.quote.toLowerCase().includes(el))) {
         console.log('I contained a no-no word so Im going again')
         return this.ngOnInit()
       };
-      this.quote = payload.quote;
+      this.quotes.push(payload.quote);
+    })
+
+
+    this.yeService.getYe().subscribe(payload => {
+      if (check.some(el => payload.quote.toLowerCase().includes(el))) {
+        console.log('I contained a no-no word so Im going again')
+        return this.ngOnInit()
+      };
+      this.quotes.push(payload.quote);
+
     })
 
 
